f885e31c808de18e126bf961b162830cd285352b
Maya Islambouli <43270477+MayaIsla@users.noreply.github.com>
2022-08-08 11:02:44 -0700

    Update testtest.ps1

--- testtest.ps1
+++ testtest.ps1
@@ -23,3 +23,22 @@ Sleep 20
 Write-Log "Script Stopped and completed"
 
 
+<#$TranscriptFile = "D:\Desktop\Scripts\Transcript\test.txt" #file path in which log/txt exists
+
+Start-Transcript -Path $TranscriptFile #start writing transcript
+
+Write-Host "Hello World" #example output
+
+Stop-Transcript #stops writing transcript
+
+
+$Transcript = Get-Content $TranscriptFile #gets the text content inside the file 
+
+$StartTime = ($Transcript -match "Start time: " -replace "Start time: ","")#matches start time 
+
+If ($StartTime) { #if the text matches after it parses for "Start time" replace the date-time format to the correct indentation
+    $NewStartTime = ([datetime]::ParseExact($StartTime, "yyyyMMddHHmmss", (Get-Culture).DateTimeFormat))
+    $NewStartTime = Get-Date($NewStartTime) -format "MM-dd-yyyy HH:mm:ss"
+    $Transcript -replace $StartTime,$NewStartTime | Out-File $TranscriptFile
+}#>
+
